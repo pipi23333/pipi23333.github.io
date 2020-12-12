cf075d49ef305fbf0d94cd445609872bf95210bd
pipi23333 <53890859+pipi23333@users.noreply.github.com>
2020-12-12 16:31:01 +0800

    Update member.js

--- choujiang/js/member.js
+++ choujiang/js/member.js
@@ -2,5 +2,69 @@ var member = [
   {
     "phone": "小朋友",
     "name": "陈墨"
+  }，
+  {
+    "phone": "小朋友",
+    "name": "林凌羽"
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
+  }，
+  {
+    "phone": "小朋友",
+    "name": ""
   }
   ]
